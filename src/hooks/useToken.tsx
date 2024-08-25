2e353af26ac1189406d6b51c1858f037a4917568
Claire Lee <claire.lee@crashco.io>
2024-08-25 20:17:36 +0900

    :bug: `useToken`이 실제 로컬스토리지의 토큰을 반환하도록

--- src/hooks/useToken.tsx
+++ src/hooks/useToken.tsx
@@ -1,7 +1,6 @@
 import useLocalStorage, { LOCAL_STORAGE_KEYS } from "./useLocalStorage";
 
 export default function useToken() {
-  // const [token] = useLocalStorage(LOCAL_STORAGE_KEYS.TOKEN);
-  // return token;
-  return 'eyJhbGciOiJIUzI1NiJ9.eyJpZCI6MiwibmFtZSI6Iu2XpOuTnOy9lOy5mCIsInJvbGUiOiJNQVNURVIiLCJpc3MiOiJGSU5BTEUiLCJpYXQiOjE3MjQyMzk2MjEsImV4cCI6MTcyNDI0MTQyMX0.Y_lFvEWAD3u3wQvGKKMHka_Ylve_toO6ss2pmxZNgJ8'
+  const [token] = useLocalStorage(LOCAL_STORAGE_KEYS.TOKEN);
+  return token;
 }
