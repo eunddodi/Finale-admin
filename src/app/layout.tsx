153d7dddb8c265ba0dea43f036a16c8932a5d730
Claire Lee <claire.lee@crashco.io>
2024-08-25 16:33:39 +0900

    ➕ Install and set react-toastify

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,12 +2,13 @@ import { Inter as FontSans } from "next/font/google"
 import "./globals.css";
 import Navigation from "./components/Navigation";
 import QueryClientProvider from "./components/QueryClientProvider";
+import 'react-toastify/dist/ReactToastify.css';
+import { ToastContainer, ToastContainerProps } from "react-toastify";
 
 const fontSans = FontSans({
   subsets: ["latin"],
   variable: "--font-sans",
 })
-
 
 export const metadata = {
   title: '관리 시스템',
@@ -23,7 +24,7 @@ export default function RootLayout({ children }: RootLayoutProps) {
     <html lang="en">
       <body className={fontSans.variable}>
         <QueryClientProvider>
-          <div className="min-h-screen bg-gray-100">
+          <div className="min-h-screen bg-white">
             <header className="bg-white shadow-sm">
               <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-4 flex justify-between items-center">
                 <h1 className="text-2xl font-bold text-gray-900">관리 시스템</h1>
@@ -34,8 +35,31 @@ export default function RootLayout({ children }: RootLayoutProps) {
               {children}
             </main>
           </div>
+          <ToastContainer {...toastConfig} />
         </QueryClientProvider>
       </body>
     </html >
   )
+}
+
+const toastConfig: ToastContainerProps = {
+  hideProgressBar: true,
+  position: 'bottom-center',
+  autoClose: 3000,
+  role: 'success',
+  toastStyle: {
+    width: '320px',
+    maxWidth: '90%',
+    boxSizing: 'border-box',
+    margin: 'auto',
+    borderRadius: '4px',
+    display: 'flex',
+    alignItems: 'center',
+    top: '12px'
+  },
+  bodyStyle: {
+    fontSize: '14px',
+    padding: 0,
+    margin: 0,
+  },
 }
