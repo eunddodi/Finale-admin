d5ece2ec072a84b57441e2db1bf493a4d79ec755
Claire Lee <claire.lee@crashco.io>
2024-08-28 21:38:49 +0900

    🚸 학생 목록 empty state 추가

--- src/app/components/StudentsList.tsx
+++ src/app/components/StudentsList.tsx
@@ -8,7 +8,7 @@ interface StudentsListProps {
 }
 
 export default function StudentsList({ searchName, data, type }: StudentsListProps) {
-
+  if (data.length === 0) return <div className="text-gray-500 py-2">No results</div>
   return (
     <ul className="space-y-2">
       {data.filter((student: StudentDetail) => !searchName || student.studentName.includes(searchName)).map(student => (
